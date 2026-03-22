c7281a19f214baffb948eb2a313f8da86d8796e1
Ops-developerRBLX <ops.discord.perso@gmail.com>
2026-03-22 23:33:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // ══════════════════════════════════════════
 //   POINT D'ENTRÉE PRINCIPAL
-//   Lance les deux modules en parallèle
 // ══════════════════════════════════════════
 require('./bienvenue.js');
 require('./commandes.js');
+require('./bot.js');
+ 
